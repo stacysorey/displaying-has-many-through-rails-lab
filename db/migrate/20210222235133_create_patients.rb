2e15d50430116f473ad3570a8a8a6eb4bed4c50f
stacysorey <stacysorey@gmail.com>
2021-02-22 18:56:12 -0500

    commit tree c7b1f72b9cde1e9691ecf5682daa0afffb5f067a

---
+++ db/migrate/20210222235133_create_patients.rb
@@ -0,0 +1,10 @@
+class CreatePatients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.datetime :created_at 
+      t.datetime :updated_at 
+      t.integer :age
+    end
+  end
+end
